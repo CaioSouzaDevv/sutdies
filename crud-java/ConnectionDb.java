0edac8a6e8cec881a451055447664b06b01b3cf1
Caiosouzadevv <caioss20.cs@gmail.com>
2026-06-06 13:06:06 -0300

    feat: estabelencendo conexão com neon postgree

--- crud-java/ConnectionDb.java
+++ crud-java/ConnectionDb.java
@@ -2,19 +2,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionDb {
-    
 
     private Connection conexao;
-        private String user = "neondb_owner";
-    private String password = "npg_dPy3iHGW1BpX";
-    
-    private String url = "jdbc:postgresql://ep-purple-water-aqr1iro4-pooler.c-8.us-east-1.aws.neon.tech/neondb?user=" + user + "&password=" + password + "&sslmode=require&channelBinding=require";
+    private String user = "neondb_owner";
+    private String password = "npg_EvOxly18PgId";
 
+    private String url = "jdbc:postgresql://host/banco?sslmode=require&channelBinding=require";
 
+    try
+    {
+        conexao = DriverManager.getConnection(url, user, password);
+        System.out.println("Conexão estabelecida com sucesso");
 
-
+    }catch(
+    Exception e)
+    {
+        System.out.println("Erro ao conectar: " + e.getMessage());
+    }
 
 }
 
-
-//Banco: jdbc:postgresql://ep-purple-water-aqr1iro4-pooler.c-8.us-east-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_dPy3iHGW1BpX&sslmode=require&channelBinding=require
+// Banco:
+// jdbc:postgresql://ep-twilight-bush-acrhyhm3-pooler.sa-east-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_EvOxly18PgId&sslmode=require&channelBinding=require
